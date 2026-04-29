0e3923086d47af9d196fd4929aa2f143b585cc11
karta.anugrah@binus.ac.id <grandsaberd1000@gmail.com>
2026-04-29 14:22:29 +0700

    Menambahkan file test.c ke repositori

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int angka1 = 10;
+    int angka2 = 20;
+    int hasil;
+
+    // Proses penjumlahan
+    hasil = angka1 + angka2;
+
+    // Menampilkan hasil
+    printf("Hasil dari %d + %d adalah: %d\n", angka1, angka2, hasil);
+
+    return 0;
+}
